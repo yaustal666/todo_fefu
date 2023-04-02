0cf8757101866495d78404dd71c21aa5ba9119d7
yaustal666 <max.poz@mail.ru>
2023-04-03 06:58:01 +1000

    taskview model class

---
+++ app/src/main/java/com/example/todo_fefu/ViewModel/TaskViewModel.kt
@@ -0,0 +1,30 @@
+package com.example.todo_fefu.ViewModel
+
+import android.app.Application
+import androidx.lifecycle.AndroidViewModel
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.viewModelScope
+import com.example.todo_fefu.data.TodoDatabase
+import com.example.todo_fefu.data.task.Task
+import com.example.todo_fefu.repository.TaskRepository
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+
+class TaskViewModel(application: Application): AndroidViewModel(application) {
+
+    private val getAllTasks: LiveData<List<Task>>
+    private val repository: TaskRepository
+
+    init {
+        val taskDao = TodoDatabase.getDatabase(application).taskDao()
+        repository = TaskRepository(taskDao)
+        getAllTasks = repository.getAllTasks
+
+    }
+
+    fun addTask(task: Task){
+        viewModelScope.launch(Dispatchers.IO){
+            repository.addTask(task)
+        }
+    }
+}
