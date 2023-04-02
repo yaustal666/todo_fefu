1e8c8d5ba660a6e6789794d208b79123008d9727
yaustal666 <max.poz@mail.ru>
2023-04-03 06:38:50 +1000

    task repository

---
+++ app/src/main/java/com/example/todo_fefu/repository/TaskRepository.kt
@@ -0,0 +1,14 @@
+package com.example.todo_fefu.repository
+
+import androidx.lifecycle.LiveData
+import com.example.todo_fefu.data.task.Task
+import com.example.todo_fefu.data.task.TaskDao
+
+class TaskRepository(private val taskDao: TaskDao) {
+
+    val getAllTasks: LiveData<List<Task>> = taskDao.getAllTasks()
+
+    suspend fun addTask(task: Task){
+        taskDao.addTask(task)
+    }
+}
